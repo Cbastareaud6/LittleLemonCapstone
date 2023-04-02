52e884de0534b1defbce82cb49966c0d125e57d9
Cathy Bastareaud <catybastareaud@gmail.com>
2023-04-02 19:05:50 -0400

    Add index html file

--- LittleLemonCapstone/LittleLemonCapstone/settings.py
+++ LittleLemonCapstone/LittleLemonCapstone/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'LittleLemonCapstone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['LittleLemonApp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +67,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'LittleLemonCapstone.wsgi.application'
 
